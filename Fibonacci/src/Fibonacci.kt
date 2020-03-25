e72e8a4bb61477843a179b0fb285a5b7efa1dad3
KoSangWon <jackoss@naver.com>
2020-03-25 20:16:50 +0900

    200325

--- Fibonacci/src/Fibonacci.kt
+++ Fibonacci/src/Fibonacci.kt
@@ -1,3 +1,13 @@
-fun main(args: Array<String>){
-    println("Hello")
+import java.math.BigInteger
+
+fun main(){
+    var n = 100
+    val first = BigInteger("0")
+    val second = BigInteger("1")
+
+    println(fibonacci(n, first, second))
+}
+
+fun fibonacci(n:Int, a:BigInteger, b:BigInteger):BigInteger{
+    return if(n==0) a else fibonacci(n-1, b, a+b)
 }
